9fc0ea7209a23b14627a0835630c1e4550a75be3
supri-untag <supri.untag@gmail.com>
2022-03-30 08:12:40 +0700

    versi udah jadi

--- app/View/user/profile.php
+++ app/View/user/profile.php
@@ -9,7 +9,7 @@
     <div class="row align-items-center g-lg-5 py-5">
         <div class="col-lg-7 text-center text-lg-start">
             <h1 class="display-4 fw-bold lh-1 mb-3">Profile</h1>
-            <p class="col-lg-10 fs-4">by <a target="_blank" href="https://www.programmerzamannow.com/">Programmer Zaman
+            <p class="col-lg-10 fs-4">learn form<a target="_blank" href="#">Programmer Zaman
                 Now</a></p>
         </div>
         <div class="col-md-10 mx-auto col-lg-5">
